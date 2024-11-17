6fe6bdfedbf1fcdf80f6fd675bc1a0cd3697f830
Armemius <rusgy@yandex.ru>
2024-11-17 16:50:30 +0300

    backend initial

---
+++ src/main/kotlin/com/armemius/lab_1_backend/Lab1BackendApplication.kt
@@ -0,0 +1,11 @@
+package com.armemius.lab_1_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab1BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<Lab1BackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/armemius/lab_1_backend/Lab1BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.armemius.lab_1_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab1BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
